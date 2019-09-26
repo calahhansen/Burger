a3033ce8c0462bad44748c5213cb535d4f1df11f
Calah Hansen <calah.hansen@gmail.com>
2019-09-25 22:21:02 -0500

    created db, table and inserted 3 seed burgers

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,10 @@
-/* write insert queries to populate the `burgers` table 
+-- write insert queries to populate the `burgers` table 
+INSERT INTO burgers (burger_name, devoured) 
 
+-- with at least three burger entries
 
-/*with at least three burger entries
+VALUES 
+("Bacon-Double-Cheeseburger", false),
+("Chicken-Bacon-Ranch-Cheeseburger", false),
+("Juicy Gouda Burger", false)
+;
